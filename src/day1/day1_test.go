fb9225cc9ef9ee6e29f4eb3c986fd15bdedd830c
Hercules Merscher <hlmerscher@gmail.com>
2019-12-01 19:54:00 +0100

    day 1 puzzle 2

--- src/day1/day1_test.go
+++ src/day1/day1_test.go
@@ -1,26 +1,52 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
-func TestMassOf(t *testing.T) {
+func TestFuelRequirement(t *testing.T) {
 	inputs := []int{12, 14, 1969, 100756}
 	outputs := []int{2, 2, 654, 33583}
 	for i := 0; i < len(inputs); i++ {
-		result := MassOf(inputs[i])
+		result := FuelRequirement(inputs[i])
 		if result != outputs[i] {
 			t.Errorf("Given input %d, output should be %d, but output is %d", inputs[i], outputs[i], result)
 		}
 	}
 }
 
-func TestSumMassOf(t *testing.T) {
+func TestFuelSum(t *testing.T) {
 	inputs := []int{12, 14, 1969, 100756}
 	outputs := []int{2, 2, 654, 33583}
 	result := 0
 	for _, n := range outputs {
 		result += n
 	}
-	sum := SumMassOf(inputs)
+	sum := FuelSum(inputs)
+	if result != sum {
+		t.Errorf("Given input %v, output should be %d, but output is %d", inputs, result, sum)
+	}
+}
+
+func TestAdditionalFuelRequirement(t *testing.T) {
+	inputs := []int{12, 1969, 100756}
+	outputs := []int{2, 966, 50346}
+	for i := 0; i < len(inputs); i++ {
+		result := AdditionalFuelRequirement(inputs[i])
+		if result != outputs[i] {
+			t.Errorf("Given input %d, output should be %d, but output is %d", inputs[i], outputs[i], result)
+		}
+	}
+}
+
+func TestAdditionalFuelSum(t *testing.T) {
+	inputs := []int{12, 1969, 100756}
+	outputs := []int{2, 966, 50346}
+	result := 0
+	for _, n := range outputs {
+		result += n
+	}
+	sum := AdditionalFuelSum(inputs)
 	if result != sum {
 		t.Errorf("Given input %v, output should be %d, but output is %d", inputs, result, sum)
 	}
