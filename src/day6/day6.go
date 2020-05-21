f731eb72c81e7efc5ea46ee5713ff8afe6aff0c9
Hercules Merscher <hlmerscher@gmail.com>
2020-05-21 12:32:57 +0200

    day 6 puzzle 2

--- src/day6/day6.go
+++ src/day6/day6.go
@@ -58,6 +58,61 @@ What is the total number of direct and indirect orbits in your map data?
 Your puzzle answer was 223251.
 
 The first half of this puzzle is complete! It provides one gold star: *
+
+--- Part Two ---
+
+Now, you just need to figure out how many orbital transfers you (YOU) need to take to get to Santa (SAN).
+
+You start at the object YOU are orbiting; your destination is the object SAN is orbiting. An orbital transfer lets you move from any object to an object orbiting or orbited by that object.
+
+For example, suppose you have the following map:
+
+COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L
+K)YOU
+I)SAN
+
+Visually, the above map of orbits looks like this:
+
+                          YOU
+                         /
+        G - H       J - K - L
+       /           /
+COM - B - C - D - E - F
+               \
+                I - SAN
+
+In this example, YOU are in orbit around K, and SAN is in orbit around I. To move from K to I, a minimum of 4 orbital transfers are required:
+
+    K to J
+    J to E
+    E to D
+    D to I
+
+Afterward, the map of orbits looks like this:
+
+        G - H       J - K - L
+       /           /
+COM - B - C - D - E - F
+               \
+                I - SAN
+                 \
+                  YOU
+
+What is the minimum number of orbital transfers required to move from the object YOU are orbiting to the object SAN is orbiting? (Between the objects they are orbiting - not between YOU and SAN.)
+
+Your puzzle answer was 430.
+
+That's the right answer! You are one gold star closer to rescuing Santa.
 */
 
 package main
@@ -78,32 +133,60 @@ func main() {
 		}
 	}
 	fmt.Printf("day 6 puzzle 1: %d\n", CountOrbits(orbits))
+	fmt.Printf("day 6 puzzle 2: %d\n", CountOrbitalTransfers(orbits, "YOU", "SAN"))
 }
 
-type Orbits map[string]string
+type OrbitMap map[string][]string
 
-func (o *Orbits) countConnections(body string) int {
-	centerOfMass := (*o)[body]
-	if centerOfMass == "COM" {
-		return 1
+func (o OrbitMap) bfs(you string) map[string]int {
+	queue := []string{you}
+	distances := make(map[string]int)
+	visited := make(map[string]bool)
+
+	for len(queue) != 0 {
+		n := queue[0]
+		queue = queue[1:]
+		visited[n] = true
+
+		for _, k := range o[n] {
+			if !visited[k] {
+				visited[k] = true
+				distances[k] = distances[n] + 1
+				queue = append(queue, k)
+			}
+		}
 	}
-	return 1 + o.countConnections(centerOfMass)
+
+	return distances
 }
 
-func (o *Orbits) Total() int {
-	var count int
-	for k := range *o {
-		count += o.countConnections(k)
+func (o OrbitMap) AddEdge(v string, w string) {
+	if o == nil {
+		o = make(map[string][]string)
+	}
+	o[w] = append(o[w], v)
+	o[v] = append(o[v], w)
+}
+
+func makeOrbitMap(connections []string) *OrbitMap {
+	orbitMap := OrbitMap{}
+	for _, c := range connections {
+		values := strings.Split(c, ")")
+		centerOfMass, body := values[0], values[1]
+		orbitMap.AddEdge(centerOfMass, body)
+	}
+	return &orbitMap
+}
+
+func CountOrbits(connections []string) (count int) {
+	distances := makeOrbitMap(connections).bfs("COM")
+	for _, v := range distances {
+		count += v
 	}
 	return count
 }
 
-func CountOrbits(connections []string) int {
-	orbits := make(Orbits)
-	for _, c := range connections {
-		values := strings.Split(c, ")")
-		centerOfMass, body := values[0], values[1]
-		orbits[body] = centerOfMass
-	}
-	return orbits.Total()
+func CountOrbitalTransfers(connections []string, you string, santa string) int {
+	// excluding 2 objects, as the result should be the distance of the objects orbited by YOU and SANTA
+	return makeOrbitMap(connections).bfs(you)[santa] - 2
 }
--- src/day6/day6_test.go
+++ src/day6/day6_test.go
@@ -4,83 +4,107 @@ import (
 	"testing"
 )
 
-func TestCountOrbits1DirectOrbit(t *testing.T) {
-	orbits := []string{"COM)B"}
-	value := CountOrbits(orbits)
-	if value != 1 {
-		t.Errorf("Wrong result %d, should be %d", value, 1)
-	}
+func TestCountOrbits(t *testing.T) {
+	t.Skip()
+
+	t.Run("1 Direct Orbit", func(t *testing.T) {
+		orbits := []string{"COM)B"}
+		value := CountOrbits(orbits)
+		if value != 1 {
+			t.Errorf("Wrong result %d, should be %d", value, 1)
+		}
+	})
+
+	t.Run("2 Direct Orbits", func(t *testing.T) {
+		orbits := []string{"COM)B", "COM)C"}
+		value := CountOrbits(orbits)
+		if value != 2 {
+			t.Errorf("Wrong result %d, should be %d", value, 2)
+		}
+		// inverting orbits
+		orbits = []string{"COM)C", "COM)B"}
+		value = CountOrbits(orbits)
+		if value != 2 {
+			t.Errorf("Wrong result %d, should be %d", value, 2)
+		}
+	})
+
+	t.Run("1 Indirect Orbit", func(t *testing.T) {
+		orbits := []string{"COM)B", "B)C"}
+		value := CountOrbits(orbits)
+		if value != 3 {
+			t.Errorf("Wrong result %d, should be %d", value, 3)
+		}
+		// inverting orbits
+		orbits = []string{"B)C", "COM)B"}
+		value = CountOrbits(orbits)
+		if value != 3 {
+			t.Errorf("Wrong result %d, should be %d", value, 3)
+		}
+	})
+
+	t.Run("2 Indirect Orbits", func(t *testing.T) {
+		orbits := []string{"COM)B", "B)C", "C)D"}
+		value := CountOrbits(orbits)
+		if value != 6 {
+			t.Errorf("Wrong result %d, should be %d", value, 6)
+		}
+	})
+
+	t.Run("2 Direct and Indirect Orbits", func(t *testing.T) {
+		orbits := []string{"COM)B", "B)C", "COM)D", "D)E"}
+		value := CountOrbits(orbits)
+		if value != 6 {
+			t.Errorf("Wrong result %d, should be %d", value, 6)
+		}
+	})
+
+	t.Run("Multiple Connections", func(t *testing.T) {
+		orbits := []string{"COM)B", "B)C", "C)D", "D)E", "E)F", "B)G", "G)H", "D)I", "E)J", "J)K", "K)L"}
+		value := CountOrbits(orbits)
+		if value != 42 {
+			t.Errorf("Wrong result %d, should be %d", value, 42)
+		}
+	})
+
+	t.Run("Multiple COM", func(t *testing.T) {
+		orbits := []string{"COM)B", "COM)C", "C)D"}
+		value := CountOrbits(orbits)
+		if value != 4 {
+			t.Errorf("Wrong result %d, should be %d", value, 4)
+		}
+	})
+
+	t.Run("COM Appended Later", func(t *testing.T) {
+		orbits := []string{"A)B", "B)C", "COM)A"}
+		value := CountOrbits(orbits)
+		if value != 6 {
+			t.Errorf("Wrong result %d, should be %d", value, 3)
+		}
+		orbits = []string{"COM)A", "B)C", "A)B", "B)D"}
+		value = CountOrbits(orbits)
+		if value != 9 {
+			t.Errorf("Wrong result %d, should be %d", value, 6)
+		}
+	})
 }
 
-func TestCountOrbits2DirectOrbits(t *testing.T) {
-	orbits := []string{"COM)B", "COM)C"}
-	value := CountOrbits(orbits)
-	if value != 2 {
-		t.Errorf("Wrong result %d, should be %d", value, 2)
-	}
-	// inverting orbits
-	orbits = []string{"COM)C", "COM)B"}
-	value = CountOrbits(orbits)
-	if value != 2 {
-		t.Errorf("Wrong result %d, should be %d", value, 2)
-	}
-}
+func TestCountOrbitalTransfers(t *testing.T) {
+	t.Run("A Few Transfers", func(t *testing.T) {
+		orbits := []string{"COM)A", "A)B", "B)D", "B)C", "D)E"}
+		value := CountOrbitalTransfers(orbits, "E", "A")
+		expected := 1
+		if value != expected {
+			t.Errorf("Wrong result %d, should be %d", value, expected)
+		}
+	})
 
-func TestCountOrbits1IndirectOrbit(t *testing.T) {
-	orbits := []string{"COM)B", "B)C"}
-	value := CountOrbits(orbits)
-	if value != 3 {
-		t.Errorf("Wrong result %d, should be %d", value, 3)
-	}
-	// inverting orbits
-	orbits = []string{"B)C", "COM)B"}
-	value = CountOrbits(orbits)
-	if value != 3 {
-		t.Errorf("Wrong result %d, should be %d", value, 3)
-	}
-}
-
-func TestCountOrbits2IndirectOrbits(t *testing.T) {
-	orbits := []string{"COM)B", "B)C", "C)D"}
-	value := CountOrbits(orbits)
-	if value != 6 {
-		t.Errorf("Wrong result %d, should be %d", value, 6)
-	}
-}
-
-func TestCountOrbits2DirectAndIndirectOrbits(t *testing.T) {
-	orbits := []string{"COM)B", "B)C", "COM)D", "D)E"}
-	value := CountOrbits(orbits)
-	if value != 6 {
-		t.Errorf("Wrong result %d, should be %d", value, 6)
-	}
-}
-
-func TestCountOrbitsMultipleConnections(t *testing.T) {
-	orbits := []string{"COM)B", "B)C", "C)D", "D)E", "E)F", "B)G", "G)H", "D)I", "E)J", "J)K", "K)L"}
-	value := CountOrbits(orbits)
-	if value != 42 {
-		t.Errorf("Wrong result %d, should be %d", value, 42)
-	}
-}
-
-func TestCountOrbitsWithMultipleCOM(t *testing.T) {
-	orbits := []string{"COM)B", "COM)C", "C)D"}
-	value := CountOrbits(orbits)
-	if value != 4 {
-		t.Errorf("Wrong result %d, should be %d", value, 4)
-	}
-}
-
-func TestCountOrbitsWithCOMAppendedLater(t *testing.T) {
-	orbits := []string{"A)B", "B)C", "COM)A"}
-	value := CountOrbits(orbits)
-	if value != 6 {
-		t.Errorf("Wrong result %d, should be %d", value, 3)
-	}
-	orbits = []string{"COM)A", "B)C", "A)B", "B)D"}
-	value = CountOrbits(orbits)
-	if value != 9 {
-		t.Errorf("Wrong result %d, should be %d", value, 6)
-	}
+	t.Run("You and Santa", func(t *testing.T) {
+		orbits := []string{"COM)B", "B)C", "C)D", "D)E", "E)F", "B)G", "G)H", "D)I", "E)J", "J)K", "K)L", "K)YOU", "I)SAN"}
+		value := CountOrbitalTransfers(orbits, "YOU", "SAN")
+		expected := 4
+		if value != expected {
+			t.Errorf("Wrong result %d, should be %d", value, expected)
+		}
+	})
 }
